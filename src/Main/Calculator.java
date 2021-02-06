3bc74d609a1f985d84ef30fe8ce3be852385e08d
Levin Ivan <odon7ron@yandex.ru>
2021-02-06 21:22:40 +0300

    v 2.0

--- src/Main/Calculator.java
+++ src/Main/Calculator.java
@@ -4,12 +4,14 @@ public class Calculator {
 
     Calculator(String str, int a, int b)
     {
+        if(a <= 0 || a > 10 || b > 10 || b <= 0) throw new IllegalArgumentException();
         call(str, a,b);
     }
 
     Calculator(String str, String a, String b)
     {
        RomNumber rim = new RomNumber(a,b);
+        if(rim.getIa() > 10 || rim.getIb() > 10) throw new IllegalArgumentException();
        call(str, rim.getIa(), rim.getIb());
        RomConvert conv = new RomConvert();
        res = conv.ArabicToRom(result);
@@ -41,6 +43,8 @@ public class Calculator {
             case "*":
                 result = a * b;
                 break;
+            default:
+                    throw new IllegalArgumentException();
         }
     }
 }
--- src/Main/StartMain.java
+++ src/Main/StartMain.java
@@ -3,25 +3,42 @@ package Main;
 import java.util.Scanner;
 import java.lang.String;
 public class StartMain {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
       Scanner sc = new Scanner(System.in);
       String str, sa, sb;
-      int a, b, result;
+      int a, b;
+      /*
+      Пример правильного ввода 1 + 1, требуется при неверном вводе типа 1+1 завершать программу.
+      в первом условии идет проверка на тип первого символа если он типа int он идёт по пути вычисления с арабскими числами, во втором случае с римскими.
+      как осуществить проверку такого ввода чтобы вызвать исключение?
+             */
+      try {
 
-      if(sc.hasNextInt())
-      {
-          a = sc.nextInt();
-          str = sc.next();
-          b = sc.nextInt();
-          Calculator cal = new Calculator(str, a, b);
-          System.out.println(cal.getResult());
+          if (sc.hasNextInt())
+          {
+              a = sc.nextInt();
+              str = sc.next();
+              b = sc.nextInt();
+              Calculator cal = new Calculator(str, a, b);
+              System.out.println(cal.getResult());
+          }
+
+          else
+              {
+              sa = sc.next();
+              str = sc.next();
+              if(sc.hasNextInt()) throw new IllegalArgumentException();
+              sb = sc.next();
+              Calculator cal = new Calculator(str, sa, sb);
+              System.out.println(cal.getRes());
+              }
+
       }
-      else {
-          sa = sc.next();
-          str = sc.next();
-          sb = sc.next();
-          Calculator cal = new Calculator(str, sa, sb);
-          System.out.println(cal.getRes());
+
+      catch (Exception e)
+      {
+          System.exit(0);
       }
     }
 }
